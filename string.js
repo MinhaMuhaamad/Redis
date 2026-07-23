6d166277b7a41dfed848916495f0caa5094a219d
MinhaMuhammad <minhaghulammuhammad@gmail.com>
2026-07-24 03:04:55 +0500

    implementing redis server using docker

---
+++ string.js
@@ -0,0 +1,7 @@
+const client = require('./client');
+async function init() {
+    await client.set("msg:6", "Heloo from Redis");
+    const result = await client.get('msg:6');
+    console.log("Result", result);
+}
+init();
